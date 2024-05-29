556a79539704a4ccba82569e6044cfc9b2e591c3
Jorge Meneses <menesesjorge2003@gmail.com>
2024-05-28 19:12:43 -0600

    Solicitud de parametros para tabla de pagos

--- src/LoansInfoMode.cpp
+++ src/LoansInfoMode.cpp
@@ -1,17 +1,31 @@
 #include "LoansInfoMode.hpp"
 
 const void getGeneralLoanInfo(){
+    // Opciones del menú
     int loanType, currency;
+
+    // Char que indica si se personalzia la tabla de pagos
+    char customTable; 
+
+    // Variables para parámetros de la tabla de pagos
+    float rate; // Tasa de interes
+    int payments = 240, amount; // Cantidad de cuotas y monto
+
     do{
+        // Encabezado del menu
         cout << "------------------------------------------------" << endl;
         cout << "--------INFORMACIÓN GENERAL DE PRÉSTAMOS--------" << endl;
         cout << "------------------------------------------------" << endl;
-        cout << "En cuál tipo de moneda desea cotizar el préstamo?" << endl;
+
+        // Preguntar tipo de moneda
+        cout << "¿En cuál tipo de moneda desea cotizar el préstamo?" << endl;
         cout << "\t1. Dólares ($)" << endl;
         cout << "\t2. Colones (₡)" << endl;
         cout << "Ingrese su eleccion: ";
         cin >> currency;
         cout << endl;
+
+        // Preguntar tipo de prestamo
         cout << "\nTipos de préstamo:" << endl;
         cout << "\t1. Préstamo personal." << endl;
         cout << "\t2. Préstamo prendario." << endl;
@@ -20,6 +34,8 @@ const void getGeneralLoanInfo(){
         cout << "Ingrese su eleccion: ";
         cin >> loanType;
         cout << endl;
+
+        // Sentencia switch-case para el tipo de préstamo
         switch (loanType){
             case PERSONAL:
                 break;
@@ -38,6 +54,34 @@ const void getGeneralLoanInfo(){
                 cout << "Seleccione un tipo de préstamo válido." << endl;
                 break;
         }
+
+        // TABLA DE PAGOS
+        // --------------
+        cout << "\nTABLA DE PAGOS" << endl;
+        cout << "----------------" << endl;
+        cout << "La tabla de pagos se mostrará usando los parámetros predeterminados para el tipo de préstamo seleccionado." << endl;
+        cout << "Cantidad de cuotas: "<< payments << endl;
+        cout << "Tasa de interés: "<< rate << "%" << endl;
+        cout << "Ingrese el monto del préstamo: ";
+        cin >> amount;
+        cout << endl;
+        // Pregunta si desea personalizar
+        cout << "¿Desea personalizar la tasa de interés y la cantidad de cuotas? (Y/N)" << endl;
+        cin >> customTable;
+        cout << endl;
+
+        if (customTable == 'Y' || customTable == 'y'){
+            // Pedir nueva cantidad de cuotas
+            cout << "Ingrese la cantidad de cuotas: ";
+            cin >> payments;
+            cout << endl;
+
+            // Pedir nueva tasa de interes
+            cout << "Ingrese la tasa de interés (0 - 1): ";
+            cin >> rate;
+            cout << endl;
+        }
+
     }while(loanType != RETURN);
     
 }
