2431e372e14ce688df212f6b74e0bd8b50755570
Jorge Meneses <menesesjorge2003@gmail.com>
2024-05-28 18:49:37 -0600

    Solicitud de moneda en modo información de préstamos

--- src/LoansInfoMode.cpp
+++ src/LoansInfoMode.cpp
@@ -6,7 +6,13 @@ const void getGeneralLoanInfo(){
         cout << "------------------------------------------------" << endl;
         cout << "--------INFORMACIÓN GENERAL DE PRÉSTAMOS--------" << endl;
         cout << "------------------------------------------------" << endl;
-        cout << "\nMenu:" << endl;
+        cout << "En cuál tipo de moneda desea cotizar el préstamo?" << endl;
+        cout << "\t1. Dólares ($)" << endl;
+        cout << "\t2. Colones (₡)" << endl;
+        cout << "Ingrese su eleccion: ";
+        cin >> currency;
+        cout << endl;
+        cout << "\nTipos de préstamo:" << endl;
         cout << "\t1. Préstamo personal." << endl;
         cout << "\t2. Préstamo prendario." << endl;
         cout << "\t3. Préstamo hipotecario." << endl;
@@ -14,6 +20,24 @@ const void getGeneralLoanInfo(){
         cout << "Ingrese su eleccion: ";
         cin >> loanType;
         cout << endl;
+        switch (loanType){
+            case PERSONAL:
+                break;
+
+            case PRENDARY:
+                break;
+
+            case MORTGAGE:
+                break;
+
+            case RETURN:
+                cout << "Regresando al menú principal..." << endl;
+                break;
+
+            default:
+                cout << "Seleccione un tipo de préstamo válido." << endl;
+                break;
+        }
     }while(loanType != RETURN);
     
 }
