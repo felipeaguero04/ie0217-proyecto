52e158f869c726bbcebd9ba6a0d8d5bb68457db6
Jorge Meneses <menesesjorge2003@gmail.com>
2024-05-28 19:43:57 -0600

    Funciones de información de cada tipo de préstamo y revisión de moneda

--- src/LoansInfoMode.cpp
+++ src/LoansInfoMode.cpp
@@ -1,15 +1,44 @@
 #include "LoansInfoMode.hpp"
 
 const void getPersonalLoanInfo(float* _rate){
+    // Imprimir intormacion de los préstamos personales
+    cout << "\nPRÉSTAMOS PERSONALES" << endl;
+    cout << "---------------------" << endl;
+    cout << "1. Crédito no asegurado" << endl;
+    cout << "2. Útil para gastos médicos, educación, consolidación de deudas, entre otros" << endl;
+    cout << "3. No requiere una garantía específica" << endl;
+    cout << "4. Rango común de tasas de interés: 20%" << " - 25%" << endl;
+    // Cambiar en memoria el interés predeterminado
     *_rate = 0.23;
+    cout << "\nTasa de interés predeterminada: " << (*_rate)*100 << endl;
 };
 
 const void getPrendaryLoanInfo(float* _rate){
+    // Imprimir intormacion de los préstamos prendarios
+    cout << "\nPRÉSTAMOS PRENDARIOS" << endl;
+    cout << "---------------------" << endl;
+    cout << "1. Crédito asegurado por un bien mueble" << endl;
+    cout << "2. Bien mueble: Joyas, vehículos, equipos electrónicos, etc." << endl;
+    cout << "3. Bien mueble actúa como garantía" << endl;
+    cout << "4. En caso de incumplimiento, el prestamista puede vender el bien para recuperar el monto prestado" << endl;
+    cout << "5. Rango común de tasas de interés: 15%" << " - 18%" << endl;
+    // Cambiar en memoria el interés predeterminado
     *_rate = 0.17;
+    cout << "\nTasa de interés predeterminada: " << (*_rate)*100 << endl;
 };
 
 const void getMortgageInfo(float* _rate){
+    // Imprimir intormacion de los préstamos hipotecarios
+    cout << "\nPRÉSTAMOS HIPOTECARIOS" << endl;
+    cout << "-----------------------" << endl;
+    cout << "1. Crédito asegurado por un bien inmueble" << endl;
+    cout << "2. Bien inmueble: Casas, terrenos, etc." << endl;
+    cout << "3. Normalmente usaod para compra de bienes raíces" << endl;
+    cout << "4. En caso de incumplimiento, el prestamista tiene el derecho de ejecutar la hipoteca y vender la propiedad para recuperar el monto prestado" << endl;
+    cout << "5. Rango común de tasas de interés: 10%" << " - 12%" << endl;
+    // Cambiar en memoria el interés predeterminado
     *_rate = 0.11;
+    cout << "\nTasa de interés predeterminada: " << (*_rate)*100 << endl;
 };
 
 const void getGeneralLoanInfo(){
@@ -36,6 +65,20 @@ const void getGeneralLoanInfo(){
         cout << "Ingrese su eleccion: ";
         cin >> currency;
         cout << endl;
+
+        // Sentencia switch-case para el tipo de moneda
+        switch (currency){
+            case 1:
+                break;
+
+            case 2:
+                break;
+
+            default:
+                cout << "Seleccione un tipo de moneda válido." << endl;
+                goto check;
+                break;
+        }
 
         // Preguntar tipo de prestamo
         cout << "\nTipos de préstamo:" << endl;
@@ -68,13 +111,14 @@ const void getGeneralLoanInfo(){
 
             default:
                 cout << "Seleccione un tipo de préstamo válido." << endl;
+                goto check;
                 break;
         }
 
         // TABLA DE PAGOS
         // --------------
         cout << "\nTABLA DE PAGOS" << endl;
-        cout << "----------------" << endl;
+        cout << "---------------" << endl;
         cout << "La tabla de pagos se mostrará usando los parámetros predeterminados para el tipo de préstamo seleccionado." << endl;
         cout << "Cantidad de cuotas: "<< payments << endl;
         cout << "Tasa de interés: "<< rate*100 << "%" << endl;
@@ -98,8 +142,10 @@ const void getGeneralLoanInfo(){
             cout << endl;
         }
 
+        // AQUI VA LA IMPRESION DE LA TABLA DE PAGOS
 
-
+        check:
+            cout << endl;
     }while(loanType != RETURN);
 
     goback:
