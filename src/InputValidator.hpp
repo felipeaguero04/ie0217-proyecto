c95228bfece6c66d3d2ebd77b129c2b75b485c59
Ismael <IJime31@gmail.com>
2024-06-28 22:04:03 -0600

    Documentacion InputValidator
    Signed-off-by: Ismael <IJime31@gmail.com>

--- src/InputValidator.hpp
+++ src/InputValidator.hpp
@@ -7,17 +7,54 @@
 
 class InputValidator {
 public:
-    // Excepcion para que el usuario solo pueda meter numero enteros segun el menu
+     /**
+     * Valida la entrada del usuario para selecciones de menú.
+     * @param input Referencia al número entero de entrada del usuario.
+     * @return Verdadero si la entrada es válida, de lo contrario, lanza una excepción.
+     * @throws std::invalid_argument si la entrada no es un número entero válido.
+     */
     static bool menusValidatedInput(int& input);
-    // Excepcion para que el usuario ingrese un unsifned long int y que no se encicle el cin
+
+    /**
+     * Valida la entrada del usuario para enteros sin signo largos.
+     * @param input Referencia al entero sin signo largo de entrada del usuario.
+     * @return Verdadero si la entrada es válida, de lo contrario, lanza una excepción.
+     * @throws std::invalid_argument si la entrada no es un entero sin signo largo válido.
+     * @throws std::out_of_range si la entrada excede el rango de unsigned long int.
+     */
     static bool amountValidatedInput(unsigned long int& input);
-    // Excepcion para que el usuario ingrese un char Y, y o N, n
+    
+    /**
+     * Valida la entrada del usuario para caracteres 'Y' o 'N'.
+     * @param input Referencia al carácter de entrada del usuario.
+     * @return Verdadero si la entrada es válida, de lo contrario, lanza una excepción.
+     * @throws std::invalid_argument si la entrada no es 'Y', 'y', 'N' o 'n'.
+     */
     static bool ynValidationInput(char& input);
-    // Excepcion para que se ingrese entero para la cantidad de cuotas
+
+    /**
+     * Valida la entrada del usuario para pagos como enteros positivos.
+     * @param input Referencia al entero de entrada del usuario.
+     * @return Verdadero si la entrada es válida, de lo contrario, lanza una excepción.
+     * @throws std::invalid_argument si la entrada no es un entero positivo válido.
+     */
     static bool paymentsValidatedInput(int& input);
-    // Excepcion para que se ingrese un rango permitido segun el tipo de prestamo
+
+    /**
+     * Valida la entrada del usuario para tasas de interés dentro de un rango específico según el tipo de préstamo.
+     * @param input Referencia al número decimal de entrada del usuario.
+     * @param loanType Entero que representa el tipo de préstamo.
+     * @return Verdadero si la entrada es válida, de lo contrario, lanza una excepción.
+     * @throws std::invalid_argument si la entrada no es un número decimal positivo válido o si loanType no es válido.
+     */
     static bool rateValidatedInput(float& input, int loanType);
-    // Excepcion para que se ingrese un tipo de moneda de acuerdo a la logica
+
+    /**
+     * Valida la entrada del usuario para seleccionar un tipo de moneda.
+     * @param input Referencia al entero de entrada del usuario.
+     * @return Verdadero si la entrada es válida, de lo contrario, lanza una excepción.
+     * @throws std::invalid_argument si la entrada no es 1 o 2.
+     */
     static bool typeMoneyInput(int& input);
     
 };
